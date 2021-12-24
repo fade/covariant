302fdb7ed6b918793a2019145f5a6dc4d751d8c4
Brian O'Reilly <fade@deepsky.com>
2021-12-24 16:36:08 -0500

    read a csv file full of sars-cov2 infection rates
    and return it in a parsed form.

--- covariant.lisp
+++ covariant.lisp
@@ -7,6 +7,19 @@
 (in-package :covariant)
 
 
+(defun read-cov2-csv (filename)
+  "given a file of csv data in the format defined by Health Ontario, read and parse it."
+  (with-open-file (s filename :direction :input)
+   (let* ((data (cl-csv:read-csv s)))
+     data)))
+
+;; (defparameter *table-with-dates*
+;;              (vellum:transform *table*
+;;                                (vellum:bind-row (Date)
+;;                                  (if Date
+;;                                      (setf Date (net-telent-date:parse-time Date)))))
+
+;; (setf *table* (vellum:make-table :columns *))
 
 (defun -main (&optional args)
   (format t "~a~%" "I don't do much yet"))
